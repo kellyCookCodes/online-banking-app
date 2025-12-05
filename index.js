d2133411a285aa1224f136b0ea9f8b961e8a9eb0
Geewiz <106394210+kellyCookCodes@users.noreply.github.com>
2025-12-04 16:52:11 -0800

    add some js for the transaction-list and corrected 2 console errors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const accountNum = '572489446789'
 
+// SHOW/HIDE ACCOUNT NUM
 const eyeBtn = document.getElementById('see-account-num')
 const accountNoText = document.getElementById('account-no-text')
 let accountNoVisible = false
@@ -8,9 +9,45 @@ eyeBtn.addEventListener('click', () => {
   eyeBtn.classList.toggle('fa-eye-slash')
   accountNoVisible = !accountNoVisible
 
-  if(accountNoVisible) {
+  if (accountNoVisible) {
     accountNoText.innerText = accountNum
-  }else{ 
+  } else {
     accountNoText.innerText = `*********${accountNum.slice(-4)}`
   }
 })
+
+// NODES
+const transactionList = document.getElementById('transaction-list')
+
+function addTransferInToList() {
+  const listGroupItem = document.createElement('a')
+  listGroupItem.classList.add('list-group-item', 'list-group-item-action')
+
+  const topPart = document.createElement('div')
+  topPart.classList.add('d-flex', 'w-100', 'justify-content-between')
+
+  const h5 = document.createElement('h5')
+  h5.classList.add('mb-1')
+  h5.innerText = 'Direct Deposit'
+  const amount = document.createElement('div')
+  amount.classList.add('fs-5', 'text-success')
+  amount.innerText = '$12,000.00'
+
+  topPart.append(h5, amount)
+
+  const middlePart = document.createElement('p')
+  middlePart.classList.add('mb-1')
+  middlePart.innerText = 'Deposit (Credit)'
+
+  const lastPart = document.createElement('div')
+  lastPart.classList.add('d-flex', 'w-100', 'justify-content-between')
+
+  const small1 = document.createElement('small')
+  small1.innerText = '22/10/2025 10:48:08'
+  const small2 = document.createElement('small')
+  small2.innerText = 'Balance: $23,579.07'
+  lastPart.append(small1, small2)
+
+  listGroupItem.append(topPart, middlePart, lastPart)
+  transactionList.append(listGroupItem)
+}
